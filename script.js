143623cef0dbb1ae747129c9218b676021810be0
HiagoCosta1 <hiagocostazero@gmail.com>
2025-11-01 12:24:03 -0300

    html pronto

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function fecharMenu() {
     const overlay = document.getElementById("overlay");
     overlay.classList.remove("ativo");
 }
+
+function mostrarPagina(nomePagina, produtoId) {
+    const todasPaginas = document.querySelectorAll('.pagina');
+    todasPaginas.forEach(pagina => {
+        pagina.classList.remove('ativa');
+    });
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+
+    if (nomePagina === 'catalogo'){
+        renderizarProdutos();
+    }
+
+    if(nomePagina === 'detalhes' && produtoId){
+        carregarDetalhes(produtoId);
+    }
+}
+
+
